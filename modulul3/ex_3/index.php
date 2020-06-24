807e8d281c22ea2a69fc1dd0357a7eceafad2ae4
branzaniovski <64470807+branzaniovski@users.noreply.github.com>
2020-06-24 14:36:57 +0300

    ex3 modul3 status

--- modulul3/ex_3/index.php
+++ modulul3/ex_3/index.php
@@ -8,7 +8,41 @@ echo 'functie:' . $functie, "<br>";
 echo 'salariu:' . $salariu, "<br>";
 echo 'bonus:' . $bonus, "<br>";
 echo 'data angajare:' . $dataAngajare, "<br>";
+
 $angajati = explode("\n", $fisier);
+$joburi = [];
+for ($i = 0; $i < count($angajati); $i++) {
+ $s = explode(',', $angajati[$i]);
+ $joburi[] = $s[2];
+}
+
+$joburi = array_unique($joburi);
+$joburi = array_values($joburi);
+$mediiSalarii = [];
+
+for ($i = 0; $i < count($joburi); $i++) {
+ $mediiSalarii[] = medieSalarJob($joburi[$i]);
+}
+$job = array_combine($joburi, $mediiSalarii);
+
+var_dump($job);
+
+#2. Daca data vechime angajat >= 1 an, calculeaza si adauga bonusul la salariu.
+$dif = 0;
+$years = 0;
+$date1 = date('Y-m-d');
+for ($i = 0; $i < count($angajati); $i++) {
+ $z = explode(',', $angajati[$i]);
+ $dif = abs(strtotime($z[5]) - strtotime($date1));
+ $years = floor($dif / (365 * 60 * 60 * 24));
+ if ($years >= 1) {
+  echo floor($z[4]);
+  $z[3] += $z[4] % $z[3];
+ } else {
+  0;
+ }
+}
+
 function medieSalarJob(string $x): int
 {
  $angj = $GLOBALS['angajati'];
@@ -36,17 +70,3 @@ function medieSalarJob(string $x): int
  }
  echo "</br>";
 }
-$joburi = [];
-for ($i = 0; $i < count($angajati); $i++) {
- $s = explode(',', $angajati[$i]);
- $joburi[] = $s[2];
-}
-$joburi = array_unique($joburi);
-$joburi = array_values($joburi);
-$mediiSalarii = [];
-
-for ($i = 0; $i < count($joburi); $i++) {
- $mediiSalarii[] = medieSalarJob($joburi[$i]);
-}
-$job = array_combine($joburi, $mediiSalarii);
-var_dump($job);
