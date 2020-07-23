8187514147db32cb1eb3e799b1a9bafe7f5e027f
branzaniovski <64470807+branzaniovski@users.noreply.github.com>
2020-07-23 16:51:47 +0300

    aplicatie practica 1 ardere de creier :) haha

--- aplicatie_practica1/helper.php
+++ aplicatie_practica1/helper.php
@@ -1,5 +1,4 @@
 <?php
-include_once 'salveaza.php';
 //helper pentru transformarea datelor trimise la server
 function testImput($data)
 {
@@ -22,6 +21,35 @@ function singleEv(array $arrayulCalendar, array $evenimentulCurent)
     if (!in_array($arrayulCalendar, $evenimentulCurent)) {
         $arrayulCalendar[] = $evenimentulCurent;
     } else {
-        unset($_SESSION['event']);
+        return in_array($arrayulCalendar, $evenimentulCurent);
     }
+}
+//o persoana poate participa de maxim 2 ori la evenimente in aceeasi zi
+//foreach de la fiecare sa ia dataS si sa faca array cu cheia dataS si participantii
+
+#toate variabilele sunt nume alias pentru parametrii array-ului $calendar din salveaza.php
+function grupareDate(array $calendarul, $data, $persoane): void
+{
+    $grupareDate = [];
+    $dataCurenta = '';
+
+    foreach ($calendarul as $key) {
+        #adaptarea parametrilor importati la functie
+        $key['data'] = $data;
+        $key['persoane'] = $persoane;
+        #conectarea
+        $dataCurenta = $key['data'];
+        $grupareDate[$dataCurenta][] = $key['persoane'];
+    }
+    //var_dump($grupareDate[$dataCurenta]);
+    //var_dump($persoane);
+
+    foreach ($grupareDate[$dataCurenta] as $pozitie => $persoana) {
+        //var_dump($persoane);
+        if ($data === $dataCurenta) {
+            $persoane = array_merge( /* blocaj */$persoane);
+        }
+
+    }
+    var_dump($persoane);
 }
--- aplicatie_practica1/salveaza.php
+++ aplicatie_practica1/salveaza.php
@@ -8,18 +8,19 @@ verificareDate($x)
  */
 
 $calendar['events'] = [];
-
-$event = [];
-
 $event['nume'] = verificareDate($_POST['evName']);
 $event['dataS'] = verificareDate($_POST['evStartDate']);
 $event['dataF'] = verificareDate($_POST['evFinishDate']);
 $event['descriere'] = verificareDate($_POST['eventDescription']);
 $event['participanti'] = $_POST['persoana'];
 
-$calendar['events'] = $event;
+$calendar['events'][] = $event;
+$calendar['events'][] = $event; //aici testam dubland arrayul sa vad daca merge foreachul din helper.phph =)
 var_dump($calendar['events']);
 
 //Un eveniment nu poate exista de mai multe ori în sesiune:
 $numeEv = $event['nume'];
 singleEv($calendar, $event);
+//o persoana poate participa de maxim 2 ori la evenimente in aceeasi zi
+//foreach de la fiecare sa ia dataS si sa faca array cu cheia dataS si participantii
+grupareDate($calendar['events'], $event['dataS'], $event['participanti']);
